0551f3c65b6936afc56728da2b2e55b85ee025f0
n87867 <luaoraa@gruposantander.com>
2026-01-28 10:20:46 +0100

    Quick Deploy: 28-01-2026 10:19

---
+++ chunk-3JFOGRNK.js
@@ -0,0 +1,9 @@
+import {
+  AppComponent
+} from "./chunk-5ZHTPVFV.js";
+import "./chunk-5H2O3ZAR.js";
+import "./chunk-LISPRQHC.js";
+import "./chunk-NDWNGLTK.js";
+export {
+  AppComponent
+};
---
+++ chunk-OY7OKSHQ.js
@@ -0,0 +1,7 @@
+import {
+  WorkerManagementComponent
+} from "./chunk-LISPRQHC.js";
+import "./chunk-NDWNGLTK.js";
+export {
+  WorkerManagementComponent
+};
