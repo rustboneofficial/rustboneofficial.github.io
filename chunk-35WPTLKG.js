d66de74b221edc12586d7904d95f64854ac0bf76
n87867 <luaoraa@gruposantander.com>
2026-02-03 15:09:16 +0100

    Visual Changes

--- chunk-35WPTLKG.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-F4BTZPYO.js";import"./chunk-Z2KP3FFN.js";export{a as WorkerManagementComponent};
--- chunk-PCDNMRK2.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-VTDESRPF.js";import"./chunk-Z2KP3FFN.js";export{a as ReaderComponent};
